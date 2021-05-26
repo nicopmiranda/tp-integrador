bd70e88623f18e5a81d22a9af8bd68035bc4aabb
altman-44 <altman.nicolas44@gmail.com>
2021-05-26 00:38:32 -0300

    Added header component with logo, input search and navigation links

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,32 +1,33 @@
 <template>
-  <div id="app">
-    <header class="header">
-      
-    </header>
-    <main class="main">
-
-    </main>
-    <footer class="footer">
-      
-    </footer>
-  </div>
+    <div id="app">
+        <the-header></the-header>
+        <main class="main"></main>
+        <footer class="footer"></footer>
+    </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader'
+
 export default {
-  name: 'App',
-  components: {
-    
-  }
+    name: 'App',
+    components: {
+        TheHeader
+    }
 }
 </script>
 
 <style>
+:root {
+  --primary-color: rgb(17, 0, 95);
+  --secondary-color: rgb(243, 233, 179);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
---
+++ web/src/components/TheHeader.vue
@@ -0,0 +1,107 @@
+<template>
+    <header>
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <div class="navbar-header">
+                <a class="navbar-brand">
+                    <img src="/favicon.ico" alt="Logo" class="logo-img" />
+                </a>
+                <div class="navbar-search">
+                    <input
+                        type="search"
+                        class="form-control"
+                        placeholder="Buscar productos"
+                    />
+                    <button class="btn btn-outline-success" type="submit">
+                        Buscar
+                    </button>
+                </div>
+                <button
+                    class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarHiddenContent"
+                    aria-controls="navbarHiddenContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div
+                class="collapse navbar-collapse navbar-body"
+                id="navbarHiddenContent"
+            >
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Registrarse</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'TheHeader'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    align-items: flex-start;
+}
+
+.navbar-header {
+    grid-column: 1 / 13;
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+}
+
+.navbar-body {
+    grid-column: 1 / 13;
+}
+
+.navbar-brand {
+    grid-column: 1 / 2;
+    padding: 0.4rem;
+}
+
+.navbar-search {
+    grid-column: 2 / 13;
+    padding: 0.4rem;
+    display: flex;
+}
+
+.navbar-nav {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    list-style-type: none;
+    padding: 0;
+}
+
+.nav-item {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+.nav-link {
+    color: var(--secondary-color);
+}
+.nav-link:hover {
+    text-shadow: 0 0 4px black;
+}
+</style>
