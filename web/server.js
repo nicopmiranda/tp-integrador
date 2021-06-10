71086b88a58d6fb9cdf4d2326bd720badc4ce5ea
altman-44 <altman.nicolas44@gmail.com>
2021-06-10 08:50:22 -0300

    -DEL: Deleted Products.spec.js; -MOD: Commented all web/server.js

--- web/server.js
+++ web/server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
@@ -11,3 +12,4 @@ app.use(serveStatic(path.join(__dirname, 'dist')))
 
 const port = process.env.PORT || 80
 app.listen(port)
+*/
