93997966422905ef359fc339d3a27d782a05c66b
altman-44 <altman.nicolas44@gmail.com>
2021-06-28 22:39:17 -0300

    -ADD: Added TheAlert component

---
+++ web/src/components/TheAlert.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="alert">
+		<p>{{ message }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'TheAlert',
+	props: {
+		message: {
+			type: String,
+			required: true
+		}
+	}
+};
+</script>
+
+<style>
+  p {
+    margin: 0;
+  }
+</style>
