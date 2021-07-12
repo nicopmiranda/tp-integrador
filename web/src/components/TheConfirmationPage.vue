1d33a9540c2237aac35f96feba6de9a02b93d34f
altman-44 <altman.nicolas44@gmail.com>
2021-07-12 20:29:52 -0300

    -ADD: Added confirmation page

---
+++ web/src/components/TheConfirmationPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="container section">
+        <i class="fas fa-check check-icon"></i>
+        <p>Su compra se ha realizado con éxito!</p>
+        <a class="btn btn-success">Ver detalle de la compra</a>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'the-confirmation-page'
+}
+</script>
+
+<style scoped>
+.section {
+    padding: 10rem;
+}
+
+.check-icon {
+    font-size: 7rem;
+    color: green;
+    margin-bottom: 2rem;
+}
+</style>
