0e4aecd294156fe1fce3d5a178f5260e8f758ef4
altman-44 <altman.nicolas44@gmail.com>
2021-07-13 21:59:43 -0300

    -FIX: Fixed how images were being displayed in Cart and Products List

--- web/src/components/Products/index.vue
+++ web/src/components/Products/index.vue
@@ -1,3 +1,3 @@
 <template src="./Products.html"></template>
 <script src="./Products.js"></script>
-<style src="./Products.css" scoped lang="css"></style>
+<style src="./Products.css" scoped></style>
